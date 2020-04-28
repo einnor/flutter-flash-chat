e5012b98b2a8053e7ef55ffdf034849df2faddfd
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-28 17:33:50 +0300

    Reverse animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -26,10 +26,10 @@ class _WelcomeScreenState extends State<WelcomeScreen>
 
     animation = CurvedAnimation(
       parent: controller,
-      curve: Curves.decelerate,
+      curve: Curves.easeIn,
     );
 
-    controller.forward();
+    controller.reverse(from: 1.0);
     controller.addListener(() {
       setState(() {
         //
