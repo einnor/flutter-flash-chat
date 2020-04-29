89fcd6f9fbd7bea6b209ec5e6fb2683ecb737d6a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-29 16:33:52 +0300

    Testing

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -25,6 +25,7 @@ class _ChatScreenState extends State<ChatScreen> {
       final user = await _auth.currentUser();
       if (user != null) {
         loggedInUser = user;
+        print(loggedInUser.email);
       }
     } catch (e) {
       print(e);
