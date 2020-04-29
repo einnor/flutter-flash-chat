f8563ed2f6a457df09e6691e8b5afd5bb9342e7a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-29 19:23:47 +0300

    Styling bubble

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -163,6 +163,10 @@ class MessageBubble extends StatelessWidget {
         color: Colors.lightBlueAccent,
         child: Text(
           '$text from $sender',
+          style: TextStyle(
+            fontSize: 15.0,
+            color: Colors.white,
+          ),
         ),
       ),
     );
