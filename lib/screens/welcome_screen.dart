c98f349fe6dd0afd6395276882fc6714d4902afc
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-29 13:35:50 +0300

    Color tween animation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -24,19 +24,12 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       vsync: this,
     );
 
-    animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeIn,
-    );
+    animation = ColorTween(
+      begin: Colors.red,
+      end: Colors.blue,
+    ).animate(controller);
 
     controller.forward();
-//    animation.addStatusListener((status) {
-//      if (status == AnimationStatus.completed) {
-//        controller.reverse(from: 1.0);
-//      } else if (status == AnimationStatus.dismissed) {
-//        controller.forward();
-//      }
-//    });
     controller.addListener(() {
       setState(() {
         //
@@ -54,7 +47,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -67,7 +60,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100,
+                    height: 60.0,
                   ),
                 ),
                 Text(
